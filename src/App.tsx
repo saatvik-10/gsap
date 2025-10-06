bcc4146473acbc79353abe1e8cefdc07665e5589
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-06 16:37:52 +0530

    added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import Hero from "./sections/Hero"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <main>
         <Navbar />
+        <Hero />
       </main>
     </>
   )
