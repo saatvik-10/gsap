88a40c2dbfd06aa833bb9ad9e47fa2885a1e3e6e
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-06 22:15:57 +0530

    added hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,56 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
 const Hero = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create('.hero-title', { type: 'chars' });
+
+    const tl = gsap.timeline({
+      delay: 1.5,
+    });
+
+    tl.to('.hero-content', {
+      opacity: 1,
+      y: 0,
+      ease: 'power1.inOut',
+    })
+      .to(
+        '.hero-text-scroll',
+        {
+          duration: 1,
+          clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+          ease: 'circ.out',
+        },
+        '-=0.3'
+      )
+      .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: 'power2.out',
+        },
+        '-=0.3'
+      );
+  });
+
   return (
     <section className='bg-main-bg'>
       <div className='hero-container'>
-        <img
-          src='/images/hero-img.png'
+        {/* <img
+          src='/img/industrial-scene.jpg'
           alt='hero-img'
           className='absolute bottom-0 left-1/2 -translate-1/2 object-auto scale-100 md:scale-150'
+        /> */}
+        <video
+          src='/vid/rule.mp4'
+          className='absolute bottom-0 top-14 object-auto scale-100 md:scale-200 w-full'
+          autoPlay
         />
-        <div className='hero-content'>
+        <div className='hero-content opacity-0'>
           <div className='overflow-hidden'>
-            <h1 className='hero-title'>Freaking Good</h1>
+            <h1 className='hero-title'>FIGHT CLUB</h1>
           </div>
 
           <div
@@ -17,7 +58,18 @@ const Hero = () => {
               clipPath: 'polygon(50% 0, 50% 0, 50% 100%, 50% 100%)',
             }}
             className='hero-text-scroll'
-          ></div>
+          >
+            <div className='hero-subtitle'>
+              <h1>Never Talk About It!</h1>
+            </div>
+          </div>
+
+          <h2>
+            Under the lights, there&apos;s no past, no future—just you, your
+            fists, and the truth.
+          </h2>
+
+          <div className='hero-button'>Lets's Fight!</div>
         </div>
       </div>
     </section>
