4b2304eab6a670efb2004c4c432f77f34ceddbf1
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-06 23:26:00 +0530

    added msg section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import Navbar from "./components/Navbar"
-import Hero from "./sections/Hero"
+import gsap from 'gsap';
+import Navbar from './components/Navbar';
+import Hero from './sections/Hero';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Msg from './sections/Msg';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-
   return (
     <>
       <main>
         <Navbar />
         <Hero />
+        <Msg />
+        <div className="h-dvh" ></div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
