7633edc5b8de25ea81cce514c59a699033658c86
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-08 12:46:12 +0530

    gsap toggle actions

--- src/sections/FightImp.tsx
+++ src/sections/FightImp.tsx
@@ -8,15 +8,16 @@ const FightImp = () => {
       type: 'chars',
     });
 
-    const paraSplit = SplitText.create('.fight-section p', {
+    const paraSplit = SplitText.create('.fight-imp-section p', {
       type: 'words, lined',
       linesClass: 'paragraph-line',
     });
 
     const contentTL = gsap.timeline({
       scrollTrigger: {
-        trigger: '.fight-section',
+        trigger: '.fight-imp-section',
         start: 'top center',
+        toggleActions: 'play none none reverse',
       },
     });
 
@@ -37,12 +38,13 @@ const FightImp = () => {
     const titleTL = gsap.timeline({
       scrollTrigger: {
         trigger: '.fight-imp-section',
-        start: 'top 100%',
+        start: 'top 30%',
         markers: true,
+        toggleActions: 'play none none reverse',
       },
     });
 
-    titleTL.to('.fight-text-scroll', {
+    titleTL.to('.fight-imp-text-scroll', {
       duration: 1,
       opacity: 1,
       clipPath: 'polygon(100% 0, 0 0, 0 100%, 100% 100%)',
@@ -67,7 +69,7 @@ const FightImp = () => {
               style={{
                 clipPath: 'polygon(0 0, 0 0, 0 100%, 0% 100%)',
               }}
-              className='fight-text-scroll place-self-start'
+              className='fight-imp-text-scroll place-self-start'
             >
               <div className='bg-white pb-5 md:pt-0 pt-3 md:px-5 px-3 inline-block'>
                 <h2 className='text-red-900'>Silence</h2>
