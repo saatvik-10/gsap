f380071184dad2a8773a9cc8550f79d7a4b55fa7
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-10 00:32:55 +0530

    added benefits-data

---
+++ src/data/data.ts
@@ -0,0 +1,105 @@
+const fights = [
+    {
+        name: "Stand Up",
+        img: "/img/fc1.jpg",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "Don't Give Up",
+        img: "/img/fc5.jpg",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+    {
+        name: "Prove Them Wrong",
+        img: "/img/fc2.webp",
+        rotation: "md:rotate-[-8deg] rotate-0",
+    },
+    {
+        name: "You Got This",
+        img: "/img/fc4.jpg",
+        rotation: "md:rotate-[8deg] rotate-0",
+    },
+];
+
+const cards = [
+    {
+        src: "/videos/f1.mp4",
+        rotation: "rotate-z-[-10deg]",
+        name: "Madison",
+        img: "/images/p1.png",
+        translation: "translate-y-[-5%]",
+    },
+    {
+        src: "/videos/f2.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Alexander",
+        img: "/images/p2.png",
+    },
+    {
+        src: "/videos/f3.mp4",
+        rotation: "rotate-z-[-4deg]",
+        name: "Andrew",
+        img: "/images/p3.png",
+        translation: "translate-y-[-5%]",
+    },
+    {
+        src: "/videos/f4.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Bryan",
+        img: "/images/p4.png",
+        translation: "translate-y-[5%]",
+    },
+    {
+        src: "/videos/f5.mp4",
+        rotation: "rotate-z-[-10deg]",
+        name: "Chris",
+        img: "/images/p5.png",
+    },
+    {
+        src: "/videos/f6.mp4",
+        rotation: "rotate-z-[4deg]",
+        name: "Devante",
+        img: "/images/p6.png",
+        translation: "translate-y-[5%]",
+    },
+    {
+        src: "/videos/f7.mp4",
+        rotation: "rotate-z-[-3deg]",
+        name: "Melisa",
+        img: "/images/p7.png",
+        translation: "translate-y-[10%]",
+    },
+];
+
+const benefits = [
+    {
+        title: 'Train, Fight, Grow',
+        color: '#82181a',
+        bg: '#fff',
+        className: 'first-title',
+        borderColor: '#82181a'
+    },
+    {
+        title: 'Discipline and Courage',
+        color: '#030712',
+        bg: '#fff',
+        className: 'second-title',
+        borderColor: '#030712'
+    },
+    {
+        title: 'Strength Forged',
+        color: '#fff',
+        bg: '#82181a',
+        className: 'third-title',
+        borderColor: '#fff'
+    },
+    {
+        title: 'Power Within',
+        color: '#fff',
+        bg: '#030712',
+        className: 'fourth-title',
+        borderColor: '#fff'
+    }
+]
+
+export { fights, cards, benefits };
