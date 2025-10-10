10691e2d75b64b5e3b705ec6f124e83c91c7c21d
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-10 16:49:35 +0530

    initialized hype section

---
+++ src/sections/Hype.tsx
@@ -0,0 +1,13 @@
+const Hype = () => {
+  return (
+    <section className='hype-section'>
+      <div className='absolute size-full flex flex-col items-center pt-[5vw]'>
+        <h1 className='first-title'>ENDURE</h1>
+        <h1 className='second-title'>EVOLVE</h1>
+        <h1 className='third-title'>RISE</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hype;
