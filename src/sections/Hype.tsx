8820b2a1d964b44f6a9abfa5dfb5d35f5bc7fd76
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-11 20:08:28 +0530

    updated hype section scroll

--- src/sections/Hype.tsx
+++ src/sections/Hype.tsx
@@ -1,7 +1,16 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const Hype = () => {
+  useGSAP(() => {
+    gsap.set(".hype-section", {
+      marginTop: "-140vh"
+    })
+  })
+
   return (
     <section className='hype-section'>
-      <div className='absolute size-full flex flex-col items-center pt-[5vw]'>
+      <div className='absolute size-full flex flex-col items-center pt-[5vw] text-white'>
         <h1 className='first-title'>ENDURE</h1>
         <h1 className='second-title'>EVOLVE</h1>
         <h1 className='third-title'>RISE</h1>
